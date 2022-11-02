340f394f252f1c804beec5dc82e3209ca678d125
Nate Wolf <daragmas@gmail.com>
2022-11-02 14:01:40 -0400

    started add powers prerequisite filter

--- client/AddPower.py
+++ client/AddPower.py
@@ -5,18 +5,21 @@ import pandas
 from pprint import pprint as pp
 
 # pp(data)
-data = pandas.read_csv('./data/powers.csv').to_dict('records')
+# data = pandas.read_csv('./data/powers.csv').to_dict('records')
+data = pandas.read_json('./data/powers.json').to_dict('records')
 
 
-def fill_listbox(listbox, power_filter):
+def fill_listbox(listbox, power_filter, character):
+    listbox.delete(0, END)
     for index, power in enumerate(data):
-        pp(power['Prerequisites'].split(', '))
-        if power_filter == 1:
-            if power['Prerequisites'] == "None":
+        # pp(power['Prerequisites'])
+        if power_filter.get() == 1:
+            if power['Prerequisites'] == ["None"]:
+                print('No PreReq')
                 listbox.insert(index, power['Name'])
             else:
-                prereqs = power['Prerequisites'].split(', ')
-                for prereq in prereqs:
+                print(power['Prerequisites'])
+                for prereq in power['Prerequisites']:
                     pass
         else:
             listbox.insert(index, power['Name'])
@@ -25,7 +28,7 @@ def fill_listbox(listbox, power_filter):
 
 def create(root, character):
     # pp(character)
-    print('Add Power Alignment Check:', character['alignment'])
+    # print('Add Power Alignment Check:', character['alignment'])
 
     def show_details():
         for widget in details_frame.winfo_children():
@@ -46,29 +49,49 @@ def create(root, character):
         power_range = LabelFrame(details_frame, text='Range')
         sustained = LabelFrame(details_frame, text='Sustained')
         subtype = LabelFrame(details_frame, text='Subtype')
+        discipline = LabelFrame(details_frame, text='Discipline')
         description = LabelFrame(details_frame, text='Description')
 
         Label(name, text=selection['Name']).grid()
         Label(alt_names, text=selection['Alternate Names']).grid()
         Label(xp_value, text=selection['Value']).grid()
-        Label(prereqs, text=selection['Prerequisites']).grid()
+
+        if selection['Prerequisites'] != ['None']:
+            for prereq, value in selection['Prerequisites'].items():
+                if value:
+                    prereqCategory = LabelFrame(prereqs, text=prereq)
+                    if type(value) != str:
+                        for item in value:
+                            try:
+                                for characteristic, rating in item.items():
+                                    Label(prereqCategory, text=f'{characteristic}: {rating}').grid(sticky=NW)
+                            except AttributeError:
+                                Label(prereqCategory, text=item).grid(sticky=NW)
+                    else:
+                        Label(prereqCategory, text=value).grid(sticky=NW)
+                    prereqCategory.grid(sticky=NW)
+        else:
+            Label(prereqs, text='None').grid(sticky=NW)
+
         Label(action, text=selection['Action']).grid()
         Label(focus, text=selection['Focus Power']).grid()
         Label(power_range, text=selection['Range']).grid()
         Label(sustained, text=selection['Sustained']).grid()
         Label(subtype, text=selection['Subtype']).grid()
+        Label(discipline, text=selection['Discipline']).grid()
         Label(description, text=selection['Description'], wraplength=600).grid()
 
         name.grid(row=0, column=0, sticky=NW)
         alt_names.grid(row=0, column=1, sticky=NW)
-        xp_value.grid(row=2, column=0, sticky=NW)
-        prereqs.grid(row=2, column=1, sticky=NW)
-        action.grid(row=3, column=0, sticky=NW)
-        focus.grid(row=3, column=1, sticky=NW)
+        xp_value.grid(row=1, column=0, sticky=NW)
+        prereqs.grid(row=0, column=2, rowspan=5, sticky=NW)
+        action.grid(row=2, column=0, sticky=NW)
+        focus.grid(row=2, column=1, sticky=NW)
         power_range.grid(row=4, column=0, sticky=NW)
         sustained.grid(row=4, column=1, sticky=NW)
-        subtype.grid(row=8, column=0, sticky=NW)
-        description.grid(row=9, column=0, columnspan=2, sticky=NW)
+        subtype.grid(row=5, column=0, sticky=NW)
+        discipline.grid(row=1, column=1, sticky=NW)
+        description.grid(row=6, column=0, columnspan=3, sticky=NW)
         details_frame.grid(row=1, column=1, sticky=NW)
 
     powers_window = Toplevel(root)
@@ -77,18 +100,16 @@ def create(root, character):
     powers_window.geometry('800x600')
 
     powers_list = Listbox(powers_window, height=25)
-    fill_listbox(powers_list, power_filter=0)
-    power_filter = IntVar()
+    power_filter = IntVar(powers_window, 0, "powerFilter")
+    fill_listbox(powers_list, power_filter=power_filter, character=character)
     filter_prereqs = Checkbutton(powers_window,
                                  text='Filter by prerequisites',
                                  variable=power_filter,
-                                 onvalue=1, offvalue=0, command=lambda: fill_listbox(powers_list, power_filter))
+                                 onvalue=power_filter.set(1),
+                                 offvalue=power_filter.set(0),
+                                 command=lambda: fill_listbox(powers_list, power_filter, character))
 
-    # fill_listbox(powers_list, power_filter)
-
-    # for index, power in enumerate(data):
-    #     powers_list.insert(index, power['Name'])
-    # # TODO: Add Filter
+    # TODO: Add Filter
     powers_list.grid(row=1, column=0, rowspan=9, sticky=NW)
     filter_prereqs.grid(row=0, column=0, sticky=NW)
     details_frame = LabelFrame(powers_window, text='Details')
