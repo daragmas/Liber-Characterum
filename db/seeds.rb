1756f232d79dad3f5c2e3c466090c033372dc3a5
Nate Wolf <daragmas@gmail.com>
2023-02-19 16:06:52 -0500

    started postgresql database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'csv'
+
+puts 'Starting Seed'
+
+puts "Seeding Traits"
+
+csv_text = File.read('db/database_csvs/traits.csv')
+csv = CSV.parse(csv_text, :headers =>true, :encoding => 'ISO-8859-1')
+
+csv.each do |row|
+    t = Trait.create!(name: row["Name"], description: row["Description"])
+end
